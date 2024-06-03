338a51925ae78b1dc42f8fb5c446e70794ed6cfb
53able <gt_vengeance@yahoo.co.jp>
2024-06-04 01:24:04 +0900

    chore: Update npm dependency to latest stable version and add script to execute index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { program } from 'commander';
-import { concatFiles } from './concatenate';
+import { program } from "commander";
+import { concatFiles } from "./concatenate";
 
 program
   .action(concatFiles)
-  .name('concatenate')
-  .description('Concatenate files in a directory')
-  .requiredOption('-d, --dir <dir>', 'a directory')
-  .option('-t, --type <type>', 'a file type', undefined)
+  .name("concatenate")
+  .description("Concatenate files in a directory")
+  .requiredOption("-d, --dir <dir>", "a directory")
+  .option("-t, --type <type>", "a file type", undefined);
 
 program.parse();
